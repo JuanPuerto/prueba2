d32e56fe8e743d16de4abb7193747718f348d985
JuanPuerto <js.puerto11@uniandes.edu.co>
2018-11-21 18:34:23 +0000

    lo modifique

--- test.py
+++ test.py
@@ -13,5 +13,10 @@ y = np.linspace(0,10,100)
 print(x)
 print(y)
 
-plt.plot(x,y)
-plt.show()
+#plt.plot(x,y)
+
+def oscar_mata_a_miyagui(miyagui):
+    return 50000*miyagui
+
+plt.plot(x,oscar_mata_a_miyagui(x))
+plt.savefig("hola.pdf")
