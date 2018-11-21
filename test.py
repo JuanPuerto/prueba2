05be722dc2aaa4a61d96ab85740dcefc7a1c1d4f
JuanPuerto <js.puerto11@uniandes.edu.co>
2018-11-21 18:45:34 +0000

    lo modifique

--- test.py
+++ test.py
@@ -13,10 +13,18 @@ y = np.linspace(0,10,100)
 print(x)
 print(y)
 
-#plt.plot(x,y)
+plt.plot(x,y)
+plt.savefig("hola1.pdf")
 
-def oscar_mata_a_miyagui(miyagui):
+def oscar(miyagui):
     return 50000*miyagui
 
-plt.plot(x,oscar_mata_a_miyagui(x))
+plt.plot(x,oscar(x))
 plt.savefig("hola.pdf")
+
+#slicing
+
+goku = np.array(["ssj1","ssj2","ssj3","God","ssjBlue","UltraInstinct"])
+print(goku)
+print(goku[2:5])
+
